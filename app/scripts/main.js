59e4304fd6eef3f97b750f5dc1f49c7b2df5440b
Ines Tavares <ines.tavares@gmail.com>
2015-02-16 20:27:29 +0000

    Fixed JS build conflict.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,54 +5,88 @@
 
 
 
+var cart = {
+  scoreGood: 0,
+  scoreBad: 0,
+  counter: 0, // counter is either 1 or -1
+  removeFromCart: function() {
+    var curName = $(this).text();
+    $(this).remove();
+    cart.counter = -1;
+    for(var i = 0; i < productArray.length; i++) {
+      if(productArray[i]['name'] === curName) {
+        var curProduct = productArray[i];
+        cart.updateScores(curProduct);
+        curProduct.notInCart();
+      }
+    }
+  },
+  updateScores: function(obj) { // take in the current object as a param
+    if(obj.ethic === 'good') {
+      cart.scoreGood = cart.scoreGood + cart.counter;
+    } else {
+      cart.scoreBad = cart.scoreBad + cart.counter;
+    }
+    $('#number').text(cart.scoreGood + cart.scoreBad);
+  },
+  calcFinalScore: function(e) {
+    e.stopPropagation();
+    if(cart.scoreGood + cart.scoreBad < 3) {
+      $('.error').removeClass('hide');
+    } else { 
+        $('#results h3').toggleClass('hide');
+        $('.calc-score').addClass('hide');
+        $('.error').addClass('hide');
+        cart.disableClicks();
+      if (cart.scoreGood > cart.scoreBad) {
+        $('.results-message').find('div.good').toggleClass('hide');
+      } else {
+        $('.results-message').find('div.bad').toggleClass('hide');
+      }
+    }
+  },
+  disableClicks: function() {
+    // el.off('click.cart');
+    $('.product').find('button').remove();
+    $('.current-cart').off('click');
+  }
+};
+// the product
+
+function Product(el, ethic) {
+  var product = this;
+  this.el = el;
+  this.ethic = ethic;
+  this.name = el.find('h4').text();
+  this.addToCart = function() {
+    cart.counter = 1;
+    $(this).text('Item in Cart');
+    $('.current-cart').append('<li id="'+ product.id +'" class="product-in-cart">' + product.name + '</li>');
+    el.off('click.cart');  
+    cart.updateScores(product);
+  };
+  this.notInCart = function() {
+    el.find('button').text('Add to Cauldron');
+    el.on('click.cart', '.cart', this.addToCart);
+  };
+  this.el.on('click.cart', '.cart', this.addToCart);
+}
+
+var productArray = [ 
+      new Product($('#internationalAid'), 'bad'),
+      new Product($('#retirementAge'), 'bad'),
+      new Product($('#trident'),'good'),
+      new Product($('#livingWage'), 'good'),
+      new Product($('#taxThreshold'), 'bad'),
+      new Product($('#houseWarm'), 'good'),
+      new Product($('#robinhoodTax'), 'good'),
+      new Product($('#subsidiesBanks'), 'good'),
+      new Product($('#benefitsCap'), 'bad'),
+      new Product($('#taxAvoidance'), 'good')
+      ];
+
 $(document).ready(function() {
-var totalCart = 0,
-    totalGood = 0,
-    totalBad  = 0;
-
-// cart logic 
-
-var updateCart = function() {
-  $('#number').text(totalCart);
-}
-
-var addToCart = function() { // the largest function ever
-  var productName = $(this).parent('div').find('h4').text(),
-      productButton = $(this);
-  totalCart++;
-  updateCart();
-  productButton.text('Item in Cart');
-  $('.current-cart').append('<li class="product-in-cart">' + productName + '</li>');
-  productButton.removeClass('cart');
-}
-
-var removeFromCart = function() {
-  var productName = $(this).text(),
-      productInList = $('.product').find("h4:contains("+ productName +")");
-      console.log(productInList);
-      totalCart--;
-      $(this).remove();
-      updateCart();
-      productInList.parent('div').find('button').addClass('cart').text('Add Item to Cart');
-}
-
-
-// score logic 
-var calcScore = function() {
-    if(!totalBad && !totalGood) {
-      console.log("You haven't selected any products");
-    } 
-}
-
-var showImage = function() {
-  var productLegend = $(this).parent('div').find('.legend'),
-      productImage = $(this).parent('div').find('img');
-  productImage.toggleClass('op');
-  productLegend.toggleClass('invisible');
-}
-
-  $('.product').on('click.cart', '.cart', addToCart);
-  $('.current-cart').on('click.product', '.product-in-cart', removeFromCart );
-  $('.product').on('click.showImage', 'img', showImage);
-  $('#results').on('click.calcScore', '.calc-score', calcScore);
+$('#results').on('click.calcScore', '.calc-score', cart.calcFinalScore);
+$('.current-cart').on('click.removeFromCart', '.product-in-cart', cart.removeFromCart);
 });
+
